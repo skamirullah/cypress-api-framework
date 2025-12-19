b7ff525558895633c3e01b4571b4feeab3b76375
skamirullah <amirthoughts@gmail.com>
2025-12-19 19:54:27 +0530

    fixed jenkins file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("cypress");
 const { getEnvConfig } = require("./cypress/utils/envManager");
-const { allureCypressTasks } = require("allure-cypress/tasks");
 
 const isCI = process.env.CI === "true";
 
@@ -22,15 +21,14 @@ module.exports = defineConfig({
     specPattern: "cypress/e2e/api/**/*.cy.js",
 
     setupNodeEvents(on, config) {
-      // ✅ REQUIRED for allure-cypress
-      on("task", allureCypressTasks());
+      // ❌ NO allure-cypress code here
 
       // Local-only mochawesome plugin
       if (!isCI) {
         require("cypress-mochawesome-reporter/plugin")(on);
       }
 
-      // 🌍 Env handling
+      // 🌍 Environment handling
       const envName = config.env.env || "qa";
       const envConfig = getEnvConfig(envName);
 
