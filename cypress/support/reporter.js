03954342c7a97733716716b8373626774c2905e4
skamirullah <amirthoughts@gmail.com>
2025-12-19 20:21:20 +0530

    fixed jenkins file

--- cypress/support/reporter.js
+++ cypress/support/reporter.js
@@ -1,30 +1,30 @@
 export const addRequestToReport = (request) => {
-  attachToReport("API Request", request);
+    attachToReport("API Request", request);
 };
 
 export const addResponseToReport = (response) => {
-  attachToReport("API Response", response);
+    attachToReport("API Response", response);
 };
 
 const attachToReport = (title, data) => {
-  const formatted =
-    typeof data === "string" ? data : JSON.stringify(data, null, 2);
+    const formatted =
+        typeof data === "string" ? data : JSON.stringify(data, null, 2);
 
-  // ✅ Allure (CI)
-  if (isAllureAvailable()) {
-    allure.attachment(title, formatted, "application/json");
-    return;
-  }
+    // ✅ Allure (CI)
+    if (isAllureAvailable()) {
+        allure.attachment(title, formatted, "application/json");
+        return;
+    }
 
-  // ✅ Mochawesome (Local)
-  if (typeof cy.addTestContext === "function") {
-    cy.addTestContext({
-      title,
-      value: formatted
-    });
-  }
+    // ✅ Mochawesome (Local)
+    if (typeof cy.addTestContext === "function") {
+        cy.addTestContext({
+            title,
+            value: formatted
+        });
+    }
 };
 
 const isAllureAvailable = () => {
-  return typeof allure !== "undefined";
+    return typeof allure !== "undefined";
 };
